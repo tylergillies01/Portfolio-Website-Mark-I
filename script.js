51a21bff5ac6e1c2a44fbb98a34ad2b366a37136
Tyler Gillies <67166201+tylergillies01@users.noreply.github.com>
2024-04-28 19:02:10 -0400

    Added basic setup code for each of the 3 files

---
+++ script.js
@@ -0,0 +1,22 @@
+// Set up intial variables
+
+// Canvas variables
+let boardHeight = 500;
+let boardWidth = 700;
+
+// Player Variables
+let playerH = 100;
+let playerW = 150;
+let playerX = 50;
+let playerY = boardHeight - playerH;
+
+// Create struct for the sprite just to simplify things
+let sprite = {
+    x : playerX,
+    y : playerY,
+    width : playerW,
+    height : playerH
+}
+
+// Now have a function for when the page loads
+// this is going to initialize the window
