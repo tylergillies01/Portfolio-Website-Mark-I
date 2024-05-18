6f24cffc4dab55a8c6f33f00b0e3a3ef649b2c7e
Tyler Gillies <tylergillies01@gmail.com>
2024-05-18 05:35:42 -0400

    Added images for the player that change depending on the directions chosen and they also react to damage

--- ArrowGame/ArrowGameScript.js
+++ ArrowGame/ArrowGameScript.js
@@ -7,17 +7,25 @@ let boardWidth = 600;
 let context;
 
 // Player sprite variables
-let spriteH = 50;
-let spriteW = 30;
+let spriteH = 130;
+let spriteW = 270;
 let spriteX = boardWidth/2 - spriteW/2;
 let spriteY = boardHeight - spriteH;
+
+let idleImgPlayer = "Assets/wiz_idle.png"
+let leftImgPlayer = "Assets/wiz_left_v2.png"
+let rightImgPlayer = "Assets/wiz_right_v2.png"
+let upImgPlayer = "Assets/wiz_up_v2.png"
+let downImgPlayer = "Assets/wiz_down_v2.png"
 
 let sprite = {
     h : spriteH,
     w : spriteW,
     x : spriteX,
-    y : spriteY
+    y : spriteY,
+    img: idleImgPlayer
 }
+
 
 //Enemy variables
 let enemyArr = [];
@@ -41,6 +49,14 @@ let misses = 0;
 let requestID;
 let intervalID;
 
+
+
+//this varibale is used to track if you were just hit
+// this fixes the problem of the hitbox not reacting well when you take damage
+let currentlyHit = false;
+
+
+
 window.onload = function(){
     // setup board
     board = document.getElementById("board");
@@ -50,9 +66,17 @@ window.onload = function(){
     context = board.getContext("2d");
 
     // Add the sprite
-    context.fillStyle="green";
-    context.fillRect(spriteX, spriteY, spriteW, spriteH);
-    
+    // context.fillStyle="green";
+    // context.fillRect(spriteX, spriteY, spriteW, spriteH);
+    spriteImg = new Image();
+    spriteImg.src = sprite.img;
+    spriteImg.onload = function(){
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+    }
+
+
+
+
     // setup the hitbox
     context.fillStyle = "rgba(135, 206, 250, 0.5)";
     context.fillRect(hitboxX, hitboxY, hitboxW, hitboxH);
@@ -93,11 +117,17 @@ function updateScreen(){
     context.clearRect(0,0,board.width,board.height);
 
     // redraw sprite
-    context.fillStyle="green";
-    context.fillRect(spriteX, spriteY, spriteW, spriteH);
+    // context.fillStyle="green";
+    // context.fillRect(spriteX, spriteY, spriteW, spriteH);
+    context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
 
     //redraw hitbox
-    context.fillStyle = "rgba(135, 206, 250, 0.5)";
+    if(currentlyHit){
+        context.fillStyle = "rgba(139, 0, 0, 0.5)"
+    }
+    else{
+        context.fillStyle = "rgba(135, 206, 250, 0.5)";
+    }
     context.fillRect(hitboxX, hitboxY, hitboxW, hitboxH);
     
     // show the score
@@ -116,6 +146,7 @@ function updateScreen(){
         if(!inHitbox(currEnemy)){
             enemyArr.shift();
             misses += 1;
+            damageSprite();
             continue;
         }
         currEnemy.y += 3;
@@ -129,23 +160,68 @@ function updateScreen(){
 
 function hitEnemy(e){
     // get an array of the y coordinates of the enemies
+    // get an array of the directions for the enemies. this will be used to check if the correct key was pressed
     let coords = [];
+    let arrowDirs = [];
     for(let i = 0; i < enemyArr.length; i++){
         coords.push(enemyArr[i].y);
+        arrowDirs.push(enemyArr[i].direction);
     }
+
+    if(e.key == "ArrowLeft"){
+        sprite.img = leftImgPlayer;
+        spriteImg.src = sprite.img;
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+    }
+    else if(e.key == "ArrowRight"){
+        sprite.img = rightImgPlayer;
+        spriteImg.src = sprite.img;
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+    }
+    else if(e.key == "ArrowUp"){
+        sprite.img = upImgPlayer;
+        spriteImg.src = sprite.img;
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+    }
+    else if(e.key == "ArrowDown"){
+        sprite.img = downImgPlayer;
+        spriteImg.src = sprite.img;
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+    }
+
+
 
     // check if the first one is inside the hitbox
     if (coords[0] < spriteY && coords[0] > hitboxY){
-        //console.log("HIT");
+        // now check it was the correct key
+        if(e.key == "ArrowLeft" && arrowDirs[0] == "left"){
+            //console.log("HIT");
+            hits += 1;
+        }
+        else if(e.key == "ArrowRight" && arrowDirs[0] == "right"){
+            hits += 1;
+        }
+        else if(e.key == "ArrowUp" && arrowDirs[0] == "up"){
+            hits += 1;
+        }
+        else if(e.key == "ArrowDown" && arrowDirs[0] == "down"){
+            hits += 1;
+        }
+        else{
+            // MISS
+            misses += 1;
+            damageSprite();
+        }
         coords.shift();
         enemyArr.shift();
-        hits += 1;
+        
     }
     else{
-        //console.log("MISS");
+        // MISS
         coords.shift();
         enemyArr.shift();
         misses += 1;
+        damageSprite();
     }
 }
 
@@ -156,7 +232,8 @@ function generateEnemy(){
         w : enemy1Width,
         x : enemy1X,
         y : enemy1Y,
-        img : null
+        img : null,
+        direction: null
     }
 
 
@@ -164,15 +241,19 @@ function generateEnemy(){
 
     if(randnum == 1){
         enemy.img = leftImg;
+        enemy.direction = "left";
     }
     else if (randnum == 2){
         enemy.img = rightImg;
+        enemy.direction = "right";
     }
     else if (randnum == 3){
         enemy.img = upImg;
+        enemy.direction = "up";
     }
     else if (randnum == 4){
         enemy.img = downImg;
+        enemy.direction = "down";
     }
     
     enemyArr.push(enemy);
@@ -193,4 +274,46 @@ function inHitbox(currEnemy){
     else{
         return false;
     }
+}
+
+// helper function for when player misses and takes damage
+// will check what the current sprite is and set the inverted version
+function damageSprite(){
+    if(sprite.img == leftImgPlayer){
+        spriteImg.src = "Assets/wiz_left_hit.png";
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+        setTimeout(() => {
+            spriteImg.src = sprite.img;
+        }, 300);
+    }
+    else if(sprite.img == rightImgPlayer){
+        spriteImg.src = "Assets/wiz_right_hit.png";
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+        setTimeout(() => {
+            spriteImg.src = sprite.img;
+        }, 300);
+    }
+    else if(sprite.img == upImgPlayer){
+        spriteImg.src = "Assets/wiz_up_hit.png";
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+        setTimeout(() => {
+            spriteImg.src = sprite.img;
+        }, 300);
+    }
+    else if(sprite.img == downImgPlayer){
+        spriteImg.src = "Assets/wiz_down_hit.png";
+        context.drawImage(spriteImg, sprite.x, sprite.y, sprite.w, sprite.h);
+        setTimeout(() => {
+            spriteImg.src = sprite.img;
+        }, 300);
+    }
+
+    //redraw hitbox
+    currentlyHit = true;
+    context.fillStyle = "rgba(139, 0, 0, 0.5)"
+    context.fillRect(hitboxX, hitboxY, hitboxW, hitboxH);
+    setTimeout(() => {
+        currentlyHit = false;
+    }, 300);
+    
 }
